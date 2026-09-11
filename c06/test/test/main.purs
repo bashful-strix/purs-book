823095e2ac55ea8e3eff298f093a2969b98e406e
bashful-strix <19427075+bashful-strix@users.noreply.github.com>
2026-09-11 19:14:08 +0100

    [c06] exercise group 2

--- c06/test/test/main.purs
+++ c06/test/test/main.purs
@@ -5,12 +5,15 @@ import Prelude
 import Effect (Effect)
 
 import Test.Spec (describe, it, parallel, pending)
-import Test.Spec.Assertions (shouldEqual)
+import Test.Spec.Assertions (shouldEqual, shouldNotEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner.Node (runSpecAndExitProcess)
 
 import Test.Cp6.Solutions
   ( Point(..)
+
+  , Complex(..)
+  , Shape(..)
   )
 
 main :: Effect Unit
@@ -21,3 +24,55 @@ main = runSpecAndExitProcess [ consoleReporter ] $ parallel do
   describe "Show Me!" do
     it "Show Point" do
       (show $ Point { x: 1.0, y: 2.0 }) `shouldEqual` "(1.0, 2.0)"
+
+  describe "Common Type Classes" do
+    let cpx real imaginary = Complex { real, imaginary }
+
+    describe "Show Complex" do
+      it "possitve imaginary" do
+        (show $ cpx 1.0 2.0) `shouldEqual` "1.0+2.0i"
+
+      it "negative imaginary" do
+        (show $ cpx 1.0 (-2.0)) `shouldEqual` "1.0-2.0i"
+
+    describe "Eq Complex" do
+      it "equal" do
+        cpx 1.0 2.0 `shouldEqual` cpx 1.0 2.0
+
+      it "not equal" do
+        cpx 1.0 2.0 `shouldNotEqual` cpx 5.0 2.0
+
+    describe "Semiring Complex" do
+      it "add" do
+        add (cpx 1.0 2.0) (cpx 3.0 4.0) `shouldEqual` (cpx 4.0 6.0)
+
+      let v = cpx 1.2 3.4
+      it "add zero" do
+        add v zero `shouldEqual` v
+
+      it "multiply" do
+        mul (cpx 1.0 2.0) (cpx 3.0 4.0) `shouldEqual` (cpx (-5.0) 10.0)
+
+      it "multiply one" do
+        mul v one `shouldEqual` v
+
+    describe "Ring Complex" do
+      it "subtract" do
+        sub (cpx 3.0 5.0) (cpx 1.0 2.0) `shouldEqual` (cpx 2.0 3.0)
+
+    describe "Show Shape" do
+      it "circle" do
+        (show $ Circle (Point { x: 1.0, y: 2.0 }) 3.0)
+          `shouldEqual` "(Circle (1.0, 2.0) 3.0)"
+
+      it "rectangle" do
+        (show $ Rectangle (Point { x: 1.0, y: 2.0 }) 3.0 4.0)
+          `shouldEqual` "(Rectangle (1.0, 2.0) 3.0 4.0)"
+
+      it "line" do
+        (show $ Line (Point { x: 1.0, y: 2.0 }) (Point { x: 3.0, y: 4.0 }))
+          `shouldEqual` "(Line (1.0, 2.0) (3.0, 4.0))"
+
+      it "text" do
+        (show $ Text (Point { x: 1.0, y: 2.0 }) "Hello")
+          `shouldEqual` "(Text (1.0, 2.0) \"Hello\")"
--- c06/test/test/solutions.purs
+++ c06/test/test/solutions.purs
@@ -2,6 +2,12 @@ module Test.Cp6.Solutions where
 
 import Prelude
 
+import Data.Array (nub, nubEq)
+import Data.Generic.Rep (class Generic)
+import Data.Newtype (class Newtype, over2, wrap)
+import Data.Ord.Generic (genericCompare)
+import Data.Show.Generic (genericShow)
+
 -- ex 1 {{{
 
 newtype Point = Point
@@ -14,5 +20,58 @@ instance Show Point where
 
 derive newtype instance Eq Point
 derive newtype instance Ord Point
+
+-- }}}
+
+-- ex2 {{{
+
+newtype Complex = Complex
+  { real :: Number
+  , imaginary :: Number
+  }
+
+instance Show Complex where
+  show (Complex { real, imaginary }) =
+    show real <> sign <> show imaginary <> "i"
+    where
+    sign = if imaginary >= 0.0 then "+" else ""
+
+derive instance Eq Complex
+
+derive instance Newtype Complex _
+
+instance Semiring Complex where
+  add = over2 Complex add
+  zero = wrap zero
+  mul
+    (Complex { real: a, imaginary: b })
+    (Complex { real: c, imaginary: d }) = Complex
+    { real: (a * c) - (b * d)
+    , imaginary: (a * d) + (b * c)
+    }
+  one = Complex { real: 1.0, imaginary: 0.0 }
+
+derive newtype instance Ring Complex
+
+data Shape
+  = Circle Point Number
+  | Rectangle Point Number Number
+  | Line Point Point
+  | Text Point String
+
+derive instance Generic Shape _
+
+instance Show Shape where
+  show = genericShow
+
+derive instance Eq Shape
+instance Ord Shape where
+  compare = genericCompare
+
+dedupShapes :: Array Shape -> Array Shape
+dedupShapes = nubEq
+
+dedupShapesFast :: Array Shape -> Array Shape
+dedupShapesFast = nub
 
 -- }}}

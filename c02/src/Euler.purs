33c7f329856c213b62e745e4690a6b255a92d600
bashful-strix <19427075+bashful-strix@users.noreply.github.com>
2026-09-11 19:14:08 +0100

    [c02] exercise group 1

--- c02/src/Euler.purs
+++ c02/src/Euler.purs
@@ -15,3 +15,10 @@ multiples n = filter (\x -> mod x 3 == 0 || mod x 5 == 0) (ns n)
 
 answer :: Int -> Int
 answer n = sum (multiples n)
+
+-- ex 1 {{{
+
+diagonal :: Number -> Number -> Number
+diagonal a b = sqrt (a * a + b * b)
+
+-- }}}
--- c02/test/Test/Main.purs
+++ c02/test/Test/Main.purs
@@ -9,7 +9,11 @@ import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
 import Test.Spec.Runner.Node (runSpecAndExitProcess)
 
-import Cp2.Euler (answer)
+import Cp2.Euler
+  ( answer
+
+  , diagonal
+  )
 
 main :: Effect Unit
 main = runSpecAndExitProcess [consoleReporter] do
@@ -19,3 +23,10 @@ main = runSpecAndExitProcess [consoleReporter] do
 
     it "below 1000" do
       answer 1000 `shouldEqual` 233168
+
+  describe "diagonal" do
+    it "3 4 5" do
+      diagonal 3.0 4.0 `shouldEqual` 5.0
+
+    it "5 12 13" do
+      diagonal 5.0 12.0 `shouldEqual` 13.0
